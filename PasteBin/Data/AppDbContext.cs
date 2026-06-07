5a24a543723e59a1bd58ee4528cdbfcac6e6822a
Tom Oddy <tom_oddy@live.co.uk>
2026-06-07 14:05:29 +0100

    Add project files.

---
+++ PasteBin/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasteBin.Models;
+
+namespace Pastebin.Data
+{
+    /// <summary>
+    /// Application database context
+    /// </summary>
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        /// <summary>
+        /// Set of pastes
+        /// </summary>
+        public DbSet<Paste> Pastes { get; set; }
+    }
+}
---
+++ PasteBin/Models/Paste.cs
@@ -0,0 +1,23 @@
+﻿namespace PasteBin.Models
+{
+    /// <summary>
+    /// Paste model
+    /// </summary>
+    public class Paste
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Content
+        /// </summary>
+        public string? Content { get; set; }
+
+        /// <summary>
+        /// Created at
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+    }
+}
